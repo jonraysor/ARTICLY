c424ebcdd9cb134f18686144f4433bc39c9a16f6
Jonathan Raysor <raysor.jonathan@gmail.com>
2019-03-25 16:49:29 -0400

    parse data and take user input

--- articly/src/main/java/org/articly/project/articly/App.java
+++ articly/src/main/java/org/articly/project/articly/App.java
@@ -1,21 +1,44 @@
 package org.articly.project.articly;
+import java.io.FileInputStream;
+
 import org.json.*;
 
-/**
- * Hello world!
- *
- */
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 public class App 
 {
     public static void main( String[] args ) throws Exception
     {
         
 
-    	System.out.println(Backend.getHTML());
+    	//System.out.println(Backend.getHTML());
+    	
+    	// create JSON object with Backend functions
 		JSONObject obj = new JSONObject(Backend.getHTML());
-		String pageName = obj.getString("copyright");
+		//String pageName = obj.getString("results");
 		
-		System.out.println(pageName);
+		// create JSON array to hold the results
+        JSONArray arr = obj.getJSONArray("results");
+        
+        // within each "result" we extract the title and print it to the screen
+        for (int i = 0; i < arr.length(); i++) {
+        	String url = arr.getJSONObject(i).getString("url");
+            String title = arr.getJSONObject(i).getString("title");
+            System.out.println(i+1 + ": title: " + title + "\n");
+            System.out.println("URL: " + url + "\n");
+		
+        }
+        
+        FileInputStream serviceAccount = new FileInputStream("/Users/jonathan/Desktop/work/School/CISC_3171/ARTICLY/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setDatabaseUrl("https://articly-753c2.firebaseio.com")
+            .build();
+
+        FirebaseApp.initializeApp(options);
         
     }
 }
+
--- articly/src/main/java/org/articly/project/articly/Backend.java
+++ articly/src/main/java/org/articly/project/articly/Backend.java
@@ -4,12 +4,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
+import java.text.MessageFormat;
 
 public class Backend {
+	
+	
+		public static int getDays() {
+			
+			Scanner reader = new Scanner(System.in);
+			System.out.println("How Many days would you like to see (1,7,30)");
+			int numDays = reader.nextInt();
+			
+			reader.close();
+			
+			return numDays;
+		}
  
 	   public static String getHTML() throws Exception {
 		   
-		   	  String urlToRead = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=Pxp8OXdQJUtbRA5n3XAN0hCpFhL6qZeb\n";
+		   	  int numDays = Backend.getDays();
+		   
+		   	  String urlToRead = MessageFormat.format("https://api.nytimes.com/svc/mostpopular/v2/viewed/{0}.json?api-key=Pxp8OXdQJUtbRA5n3XAN0hCpFhL6qZeb\n", numDays); 
 		   	  StringBuilder result = new StringBuilder();
 		      URL url = new URL(urlToRead);
 		      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -22,4 +38,8 @@ public class Backend {
 		      rd.close();
 		      return result.toString();
 	   }
+	   
+	   
+	   
+	   
 }
